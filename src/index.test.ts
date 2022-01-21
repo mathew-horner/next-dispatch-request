100b8f7ef995dc581579dc233442cfcd8f67fa8d
Mathew Horner <mathewhorner456@gmail.com>
2022-01-21 02:01:00 -0600

    Better describe block

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,17 +1,19 @@
-describe("dispatchRequest calls the correct handler", () => {
-  test("when sent GET", () => {});
-  test("when sent POST", () => {});
-  test("when sent PUT", () => {});
-  test("when sent DELETE", () => {});
+describe("dispatchRequest", () => {
+  describe("calls the correct handler", () => {
+    test("when sent GET", () => {});
+    test("when sent POST", () => {});
+    test("when sent PUT", () => {});
+    test("when sent DELETE", () => {});
+  });
+
+  describe("sends a 405 Not Allowed response and enumerates the allowed methods", () => {
+    test("when sent GET without a GET handler", () => {});
+    test("when sent POST without a POST handler", () => {});
+    test("when sent PUT without a PUT handler", () => {});
+    test("when sent DELETE without a DELETE handler", () => {});
+  });
+
+  test("sends a 400 Bad Request response when given an undefined method", () => {});
+
+  test("sends a 500 Internal Server Response with the error message when an error occurs in the handler", () => {});
 });
-
-describe("dispatchRequest sends a 405 Not Allowed response and enumerates the allowed methods", () => {
-  test("when sent GET without a GET handler", () => {});
-  test("when sent POST without a POST handler", () => {});
-  test("when sent PUT without a PUT handler", () => {});
-  test("when sent DELETE without a DELETE handler", () => {});
-});
-
-test("dispatchRequest sends a 400 Bad Request response when given an undefined method", () => {});
-
-test("dispatchRequest sends a 500 Internal Server Response with the error message when an error occurs in the handler", () => {});
